fa15b63c699deb5f9532b9dfdc93b54aa8840035
dmelcher <danmelcher1@gmail.com>
2019-01-14 17:14:17 -0600

    Initial load

---
+++ Dashboards/js/all-contants.js
@@ -0,0 +1,8 @@
+var acVolume="Default Volume";
+var BDAuser="glory";
+var defaultAcType="viewer";
+var defaultAcDisplay1="notoolbar";
+var defaultAcDisplay2="none";
+var defaultAcName="mashboard1.rptdesign";
+var defaultAcStyle="none";
+var TRANSLATED_USER= [["martin","fr"], ["thomas","de"]];
